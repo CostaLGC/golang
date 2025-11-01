300beaecd62a442d73837dd16f0f88bae65e07ff
costalgc <lg2costa@gmail.com>
2025-11-01 12:43:51 -0300

    second commit

---
+++ 4.36.naming_conventions/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Pascal case
+	// Eg. CalculateArea, UserInfo, NewHTTPRequest
+	// Structs, interfaces, enums
+
+	// snake_case
+	// palavras escricas em lower case mas separadas por underscorre
+	// Eg. user_id, first_name, https_request
+
+
+	// UPPERCASE
+	// use case is contance 
+
+
+	// mixedCase
+	// Eg. javaScript, htmlDocument, isValid
+}
